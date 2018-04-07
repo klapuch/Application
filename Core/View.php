b7b805a91ef1642087e3533a631db90bdfd85a82
Dominik Klapuch <klapuchdominik@gmail.com>
2018-04-07 23:36:08 +0200

    View changed method

--- Core/View.php
+++ Core/View.php
@@ -2,12 +2,10 @@
 declare(strict_types = 1);
 namespace Klapuch\Application;
 
-use Klapuch\Output;
-
 interface View {
 	/**
 	 * @param array $input
-	 * @return \Klapuch\Output\Template
+	 * @return \Klapuch\Application\Response
 	 */
-	public function template(array $input): Output\Template;
+	public function response(array $input): Response;
 }
